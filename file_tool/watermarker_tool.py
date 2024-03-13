257e068aea0d8a789475ab8233e0c8674f733df8
zhangningboo <zhangningbo21@mails.ucas.ac.cn>
2024-03-13 10:26:08 +0800

    feat: 修改添加水印的图片保存路径

--- file_tool/watermarker_tool.py
+++ file_tool/watermarker_tool.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import cv2
 import numpy as np
@@ -11,10 +12,6 @@ class Watermarker:
         self.image_file = image_file
         self.watermark_text = watermark_text
         self.image_path = Path(image_file)
-        name_without_suffix = self.image_path.name.removesuffix(self.image_path.suffix)
-        self.save_file = self.image_path.parent.joinpath(f"{name_without_suffix}-加水印{self.image_path.suffix}")
-        print(f"{name_without_suffix = }, {self.image_path.parent = }, {self.save_file = }")
-
     
     def opencv_fmt(self):
         # 读取原始图片
@@ -45,15 +42,25 @@ class Watermarker:
         # 保存添加水印后的图片
         cv2.imencode('.jpg', image)[1].tofile(self.save_file.absolute().as_posix())  # 
 
-    def fmt(self, opacity=0.2, angle=45, space=30):
+    def fmt(self, save_file_path: Path=None, opacity=0.2, angle=45, space=30):
+        if save_file_path is None:
+            save_file_path = self.save_file.absolute().as_posix()
         add_mark(file=self.image_path.absolute().as_posix(), 
-                 out=self.save_file.absolute().as_posix(), 
+                 out=save_file_path.absolute().as_posix(), 
                  mark=self.watermark_text, 
                  opacity=opacity, angle=angle, space=space)
 
 
 if __name__ == '__main__':
-    image_file = rf"path\to\image.jpg"
-    watermark_text = rf"marker text"
-    marker = Watermarker(image_file=image_file, watermark_text=watermark_text)
-    marker.fmt()
+    watermark_text = rf"仅用于xxx认证"
+    # 获取当前脚本的绝对路径
+    script_path = os.path.abspath(__file__)
+    # 获取当前脚本所在目录
+    script_dir = os.path.dirname(script_path)
+    save_file_path = Path(script_dir)
+    
+    marker = Watermarker(image_file=rf"path/to/image", watermark_text=watermark_text)
+    marker.fmt(save_file_path=save_file_path)
+    
+    marker = Watermarker(image_file=rf"path/to/image", watermark_text=watermark_text)
+    marker.fmt(save_file_path=save_file_path)
